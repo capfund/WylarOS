3e865cb5889ab1af5aec119abe1955a819538db7
Capfund <onions193@gmail.com>
2026-05-16 22:43:37 +0700

    [gdt]: stable

--- src/kernel.c
+++ src/kernel.c
@@ -1,8 +1,11 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include "vga/vga.h"
+#include "gdt/gdt.h"
 
 void kernel_main() {
     vga_clear();
+    vga_print("HI!\n", VGA_COLOR_WHITE, VGA_COLOR_BLACK);
+    i686_GDT_Initialize();
     vga_print("Hello, World!", VGA_COLOR_WHITE, VGA_COLOR_BLACK);
 }

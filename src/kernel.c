c0344a2be14076514d44d4ca7c71e15f93d70571
Capfund <onions193@gmail.com>
2026-05-16 23:49:03 +0700

    [idt]: testing idt / stable

--- src/kernel.c
+++ src/kernel.c
@@ -2,10 +2,12 @@
 #include <stdbool.h>
 #include "vga/vga.h"
 #include "gdt/gdt.h"
+#include "idt/idt.h"
 
 void kernel_main() {
     vga_clear();
-    vga_print("HI!\n", VGA_COLOR_WHITE, VGA_COLOR_BLACK);
     i686_GDT_Initialize();
+    i686_IDT_Initialize();
+    i686_IDT_EnableGate(0); // Enable divide by zero exception for testing
     vga_print("Hello, World!", VGA_COLOR_WHITE, VGA_COLOR_BLACK);
 }
